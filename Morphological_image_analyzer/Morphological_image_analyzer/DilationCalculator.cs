352b677e39a7b3dc2d2a44a94a36b59117120869
Pawel Janowski <pawel.janowski@guideme.cba.pl>
2018-10-22 00:40:29 +0200

    Attempt to implement dilation

---
+++ Morphological_image_analyzer/Morphological_image_analyzer/DilationCalculator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Morphological_image_analyzer
+{
+    public class DilationCalculator
+    {
+
+        private Bitmap bmpimg;
+
+        public void setImage(Bitmap bmp)
+        {
+            bmpimg = (Bitmap)bmp.Clone();
+        }
+
+        public Bitmap getImage()
+        {
+            return (Bitmap)bmpimg.Clone();
+        }
+
+        public void performMorphologicalOperation()
+        {
+
+            byte[,] sele = new byte[3, 3];
+
+                sele[0, 0] = 1;
+                sele[0, 1] = 1;
+                sele[0, 2] = 1;
+                sele[1, 0] = 1;
+                sele[1, 1] = 1;
+                sele[1, 2] = 1;
+                sele[2, 0] = 1;
+                sele[2, 1] = 1;
+                sele[2, 2] = 1;
+
+
+            Bitmap tempbmp = (Bitmap)this.bmpimg.Clone();
+            BitmapData data2 = tempbmp.LockBits(new Rectangle(0, 0, tempbmp.Width, tempbmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData data = bmpimg.LockBits(new Rectangle(0, 0, bmpimg.Width, bmpimg.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            byte[,] sElement = sele;
+
+            unsafe
+            {
+                byte* ptr = (byte*)data.Scan0;
+                byte* tptr = (byte*)data2.Scan0;
+
+                ptr += data.Stride + 3;
+                tptr += data.Stride + 3;
+
+                int remain = data.Stride - data.Width * 3;
+
+                for (int i = 1; i < data.Height - 1; i++)
+                {
+                    for (int j = 1; j < data.Width - 1; j++)
+                    {
+
+                        if (ptr[0] == 255)
+                        {
+                            byte* temp = tptr - data.Stride - 3;
+
+                            for (int k = 0; k < 3; k++)
+                            {
+                                for (int l = 0; l < 3; l++)
+                                {
+                                    temp[data.Stride * k + l * 3] = temp[data.Stride * k + l * 3 + 1] = temp[data.Stride * k + l * 3 + 2] = (byte)(sElement[k, l] * 255);
+                                }
+                            }
+                        }
+
+                        ptr += 3;
+                        tptr += 3;
+                    }
+                    ptr += remain + 6;
+                    tptr += remain + 6;
+                }
+            }
+
+            bmpimg.UnlockBits(data);
+            tempbmp.UnlockBits(data2);
+
+            bmpimg = (Bitmap)tempbmp.Clone();
+
+        }
+    }
+}
+
--- Morphological_image_analyzer/Morphological_image_analyzer/MainWindow.xaml.cs
+++ Morphological_image_analyzer/Morphological_image_analyzer/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
 
 namespace Morphological_image_analyzer
 {
@@ -20,10 +14,12 @@ namespace Morphological_image_analyzer
     /// </summary>
     public partial class MainWindow : Window
     {
-        Random rnd = new Random();
-
         static readonly int minSize = 15;
         static readonly int sizeOfWindow = 260;
+
+        DilationCalculator dilationCalculator = new DilationCalculator();
+
+        Random rnd = new Random();
 
         public MainWindow()
         {
@@ -37,7 +33,7 @@ namespace Morphological_image_analyzer
             int startPointLeft = rnd.Next(sizeOfWindow - width);
             int startPointTop = rnd.Next(sizeOfWindow - height);
 
-            Rectangle rect = new Rectangle() { Width = width, Height = height, Fill = Brushes.Transparent, StrokeThickness = 5, Stroke = Brushes.Black };
+            System.Windows.Shapes.Rectangle rect = new System.Windows.Shapes.Rectangle() { Width = width, Height = height, Fill = System.Windows.Media.Brushes.Transparent, StrokeThickness = 5, Stroke = System.Windows.Media.Brushes.Black };
             this.analizedCanvas.Children.Add(rect);
             Canvas.SetLeft(rect, startPointLeft);
             Canvas.SetTop(rect, startPointTop);
@@ -50,7 +46,7 @@ namespace Morphological_image_analyzer
             int startPointLeft = rnd.Next(sizeOfWindow - width);
             int startPointTop = rnd.Next(sizeOfWindow - height);
 
-            Ellipse ellipse = new Ellipse() { Width = width, Height = height, Fill = Brushes.Transparent, StrokeThickness = 5, Stroke = Brushes.Black };
+            Ellipse ellipse = new Ellipse() { Width = width, Height = height, Fill = System.Windows.Media.Brushes.Transparent, StrokeThickness = 5, Stroke = System.Windows.Media.Brushes.Black };
             this.analizedCanvas.Children.Add(ellipse);
             Canvas.SetLeft(ellipse, startPointLeft);
             Canvas.SetTop(ellipse, startPointTop);
@@ -63,8 +59,99 @@ namespace Morphological_image_analyzer
             int x2 = rnd.Next(minSize, sizeOfWindow);
             int y2 = rnd.Next(minSize, sizeOfWindow);
 
-            Line line = new Line() { X1 = x1, Y1 = y1, X2 = x2, Y2 = y2, StrokeThickness = 5, Stroke = Brushes.Black };
+            Line line = new Line() { X1 = x1, Y1 = y1, X2 = x2, Y2 = y2, StrokeThickness = 5, Stroke = System.Windows.Media.Brushes.Black };
             this.analizedCanvas.Children.Add(line);
+        }
+
+        void performDilation_Click(object sender, RoutedEventArgs e)
+        {
+            //RenderTargetBitmap bitmap = new RenderTargetBitmap((int)analizedCanvas.Width, (int)analizedCanvas.Height, 96d, 96d, PixelFormats.Pbgra32);
+            //bitmap.Render(analizedCanvas);
+
+            //BitmapEncoder encoder = new PngBitmapEncoder();
+            //encoder.Frames.Add(BitmapFrame.Create(bitmap));
+
+            //using (Stream stm = File.Create("C:\test\test.png"))
+            //{
+            //    encoder.Save(stm);
+            //}
+
+            //dilationCalculator.setImage(new Bitmap("C:\test\test.png"));
+            //dilationCalculator.performMorphologicalOperation();
+            //Bitmap bitmapPerformed = dilationCalculator.getImage();
+
+            //this.analizedCanvas.Children.Clear();
+
+            //Stream stm = File.Open("C:\test\test.png", FileMode.Open);
+
+            //encoder.Frames.Clear();
+
+            //analizedCanvas.Children.Add(bitmapPerformed.);
+
+            WriteableBitmap writeableBitmapFromCanvas = SaveAsWriteableBitmap(analizedCanvas);
+            Bitmap bitmapFromCanvas = BitmapFromWriteableBitmap(writeableBitmapFromCanvas);
+
+            dilationCalculator.setImage(bitmapFromCanvas);
+            dilationCalculator.performMorphologicalOperation();
+            Bitmap bitmapToCanvas = dilationCalculator.getImage();
+
+            System.Windows.Media.Imaging.BitmapSource bitmapSource =
+                System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                bitmapToCanvas.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty,
+                System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+                System.Windows.Media.Imaging.WriteableBitmap writeableBitmap = new System.Windows.Media.Imaging.WriteableBitmap(bitmapSource);
+
+            analizedCanvas.Children.Clear();
+            ImageBrush brush = new ImageBrush();
+            brush.ImageSource = bitmapSource;
+            analizedCanvas.Background = brush;
+        }
+
+        public WriteableBitmap SaveAsWriteableBitmap(Canvas surface)
+        {
+            if (surface == null) return null;
+
+            // Save current canvas transform
+            Transform transform = surface.LayoutTransform;
+            // reset current transform (in case it is scaled or rotated)
+            surface.LayoutTransform = null;
+
+            // Get the size of canvas
+            System.Windows.Size size = new System.Windows.Size(surface.ActualWidth, surface.ActualHeight);
+            // Measure and arrange the surface
+            // VERY IMPORTANT
+            surface.Measure(size);
+            surface.Arrange(new Rect(size));
+
+            // Create a render bitmap and push the surface to it
+            RenderTargetBitmap renderBitmap = new RenderTargetBitmap(
+              (int)size.Width,
+              (int)size.Height,
+              96d,
+              96d,
+              PixelFormats.Pbgra32);
+            renderBitmap.Render(surface);
+
+
+            //Restore previously saved layout
+            surface.LayoutTransform = transform;
+
+            //create and return a new WriteableBitmap using the RenderTargetBitmap
+            return new WriteableBitmap(renderBitmap);
+
+        }
+
+        private System.Drawing.Bitmap BitmapFromWriteableBitmap(WriteableBitmap writeBmp)
+        {
+            System.Drawing.Bitmap bmp;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create((BitmapSource)writeBmp));
+                enc.Save(outStream);
+                bmp = new System.Drawing.Bitmap(outStream);
+            }
+            return bmp;
         }
     }
 }
