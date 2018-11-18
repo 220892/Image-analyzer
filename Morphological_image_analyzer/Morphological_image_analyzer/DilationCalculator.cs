2d68c28c2e74d9631717dc9d0ad1eb307e6d1085
Pawel Janowski <pawel.janowski@guideme.cba.pl>
2018-11-19 00:04:17 +0100

    Moved common logic of calculators to AbstractDilationAndErosionCalculator

--- Morphological_image_analyzer/Morphological_image_analyzer/DilationCalculator.cs
+++ Morphological_image_analyzer/Morphological_image_analyzer/DilationCalculator.cs
@@ -5,88 +5,16 @@ using System.Runtime.InteropServices;
 
 namespace Morphological_image_analyzer
 {
-    public class DilationCalculator : IMorphologicalCalculator
+    public class DilationCalculator : AbstractDilationAndErosionCalculator
     {
-
-        private byte[,] shape
+        protected override byte getInitialValueForAlgorithm()
         {
-            get
-            {
-                return new byte[,]
-                {
-            { 0, 1, 0 },
-            { 1, 1, 1 },
-            { 0, 1, 0 }
-                };
-            }
+            return 255;
         }
 
-        public Bitmap performMorphologicalOperation(Bitmap srcImg)
+        protected override byte getValueFromCoreOperation(byte val1, byte val2)
         {
-            //Create image dimension variables for convenience
-            int width = srcImg.Width;
-            int height = srcImg.Height;
-
-            //Lock bits to system memory for fast processing
-            Rectangle canvas = new Rectangle(0, 0, width, height);
-            BitmapData srcData = srcImg.LockBits(canvas, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-            int stride = srcData.Stride;
-            int bytes = stride * srcData.Height;
-
-            //Create byte arrays that will hold all pixel data, one for processing, one for output
-            byte[] pixelBuffer = new byte[bytes];
-            byte[] resultBuffer = new byte[bytes];
-
-            //Write pixel data to array meant for processing
-            Marshal.Copy(srcData.Scan0, pixelBuffer, 0, bytes);
-            srcImg.UnlockBits(srcData);
-
-            int kernelSize = 3;
-            int kernelDim = kernelSize;
-
-            //This is the offset of center pixel from border of the kernel
-            int kernelOffset = (kernelDim - 1) / 2;
-            int calcOffset = 0;
-            int byteOffset = 0;
-            for (int y = kernelOffset; y < height - kernelOffset; y++)
-            {
-                for (int x = kernelOffset; x < width - kernelOffset; x++)
-                {
-                    byte value = 255;
-                    byteOffset = y * stride + x * 4;
-
-                    //Apply dilation
-                    for (int ykernel = -kernelOffset; ykernel <= kernelOffset; ykernel++)
-                    {
-                        for (int xkernel = -kernelOffset; xkernel <= kernelOffset; xkernel++)
-                        {
-                            if (shape[ykernel + kernelOffset, xkernel + kernelOffset] == 1)
-                            {
-                                calcOffset = byteOffset + ykernel * stride + xkernel * 4;
-                                value = Math.Min(value, pixelBuffer[calcOffset]);
-                            }
-                            else
-                            {
-                                continue;
-                            }
-                        }
-                    }
-                    //Write processed data into the second array
-                    resultBuffer[byteOffset] = value;
-                    resultBuffer[byteOffset + 1] = value;
-                    resultBuffer[byteOffset + 2] = value;
-                    resultBuffer[byteOffset + 3] = 255;
-                }
-            }
-
-            //Create output bitmap of this function
-            Bitmap rsltImg = new Bitmap(width, height);
-            BitmapData rsltData = rsltImg.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-
-            //Write processed data into bitmap form
-            Marshal.Copy(resultBuffer, 0, rsltData.Scan0, bytes);
-            rsltImg.UnlockBits(rsltData);
-            return rsltImg;
+            return Math.Min(val1, val2);
         }
     }
 }
--- Morphological_image_analyzer/Morphological_image_analyzer/ErosionCalculator.cs
+++ Morphological_image_analyzer/Morphological_image_analyzer/ErosionCalculator.cs
@@ -1,82 +1,17 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
 
 namespace Morphological_image_analyzer
 {
-    public class ErosionCalculator : IMorphologicalCalculator
+    public class ErosionCalculator : AbstractDilationAndErosionCalculator
     {
-
-        private byte[,] kernel
+        protected override byte getInitialValueForAlgorithm()
         {
-            get
-            {
-                return new byte[,]
-                {
-            { 0, 1, 0 },
-            { 1, 1, 1 },
-            { 0, 1, 0 }
-                };
-            }
+            return 0;
         }
 
-        public Bitmap performMorphologicalOperation(Bitmap srcImage)
+        protected override byte getValueFromCoreOperation(byte val1, byte val2)
         {
-            int width = srcImage.Width;
-            int height = srcImage.Height;
-
-            Rectangle canvas = new Rectangle(0, 0, width, height);
-            BitmapData srcData = srcImage.LockBits(canvas, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-
-            int bytes = srcData.Stride * srcData.Height;
-            byte[] pixelBuffer = new byte[bytes];
-            byte[] resultBuffer = new byte[bytes];
-
-            Marshal.Copy(srcData.Scan0, pixelBuffer, 0, bytes);
-            srcImage.UnlockBits(srcData);
-
-
-            int kernelSize = 3;
-            int kernelOffset = (kernelSize - 1) / 2;
-            int calcOffset = 0;
-            int byteOffset = 0;
-
-            for (int y = kernelOffset; y < height - kernelOffset; y++)
-            {
-                for (int x = kernelOffset; x < width - kernelOffset; x++)
-                {
-                    byte value = 0;
-                    byteOffset = y * srcData.Stride + x * 4;
-                    for (int ykernel = -kernelOffset; ykernel <= kernelOffset; ykernel++)
-                    {
-                        for (int xkernel = -kernelOffset; xkernel <= kernelOffset; xkernel++)
-                        {
-                            if (kernel[ykernel + kernelOffset, xkernel + kernelOffset] == 1)
-                            {
-                                calcOffset = byteOffset + ykernel * srcData.Stride + xkernel * 4;
-                                value = Math.Max(value, pixelBuffer[calcOffset]);
-                            }
-                            else
-                            {
-                                continue;
-                            }
-                        }
-                    }
-                    resultBuffer[byteOffset] = value;
-                    resultBuffer[byteOffset + 1] = value;
-                    resultBuffer[byteOffset + 2] = value;
-                    resultBuffer[byteOffset + 3] = 255;
-                }
-            }
-
-
-            Bitmap result = new Bitmap(width, height);
-            BitmapData resultData = result.LockBits(canvas, ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-            Marshal.Copy(resultBuffer, 0, resultData.Scan0, bytes);
-            result.UnlockBits(resultData);
-            return result;
-
+            return Math.Max(val1, val2);
         }
     }
 }
