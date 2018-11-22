ad38e1253e72de94c3c692c52ee3d81032561456
Pawel Janowski <pawel.janowski@guideme.cba.pl>
2018-11-22 20:17:42 +0100

    Implemented adding text boxes to ModalWindow as a matrix

--- Morphological_image_analyzer/Morphological_image_analyzer/ModalWindow.xaml.cs
+++ Morphological_image_analyzer/Morphological_image_analyzer/ModalWindow.xaml.cs
@@ -34,9 +34,16 @@ namespace Morphological_image_analyzer
 
             for(int i = 1; i <= Int32.Parse(size); i++)
             {
+                StackPanel stackPanelHorizontal = new StackPanel();
+                stackPanelHorizontal.Name = "panel_" + i.ToString();
+                stackPanelHorizontal.Orientation = Orientation.Horizontal;
+
+                stackPanel.Children.Add(stackPanelHorizontal);
+                stackPanel.RegisterName(stackPanelHorizontal.Name, stackPanelHorizontal);
+
                 for (int j = 1; j <= Int32.Parse(size); j++)
                 {
-                    createNewTextBox(i, j);
+                    createNewTextBox(stackPanelHorizontal, i, j);
                 }
             }
 
@@ -50,14 +57,14 @@ namespace Morphological_image_analyzer
             e.Handled = regex.IsMatch(e.Text);
         }
 
-        protected void createNewTextBox(int rowId, int columnId)
+        protected void createNewTextBox(StackPanel panel, int rowId, int columnId)
         {
             TextBox txtNumber = new TextBox();
-            //txtNumber.Name = rowId + ", " + columnId;
+            txtNumber.Name = "box_" + rowId.ToString() + "_" + columnId.ToString();
             txtNumber.Text = "1";
             txtNumber.Width = 20;
-            stackPanel.Children.Add(txtNumber);
-            //stackPanel.RegisterName(txtNumber.Name, txtNumber);
+            panel.Children.Add(txtNumber);
+            panel.RegisterName(txtNumber.Name, txtNumber);
         }
 
     }
